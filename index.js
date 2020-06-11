89dcd565f9889751e13790f6e1c9c894a566eb2d
arianahabas <arianahabas@gmail.com>
2020-06-11 14:44:41 -0500

    updated js file

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
 function is31Flavors(arr){
     if(arr.length ===31){
-return true;
+    return true;
     }else{
-return false;
+    return false;
     }
 }
 console.log(is31Flavors(originalFlavors));
@@ -61,8 +61,7 @@ Your function should add the flavor to the front of the array and console.log th
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
 function addFlavor(arr, flav){
-    arr.unshift(flav);
-    console.log(arr);
+    return arr.unshift(flav);
 }
 console.log(addFlavor(originalFlavors,'Rainbow Sherbert'));
 
@@ -77,10 +76,10 @@ console.log(addFlavor(originalFlavors,'Rainbow Sherbert'));
 // For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
 function removeLastFlavor(arr){
-    arr.pop();
-    console.log(arr)
+    return arr.pop();
  }
- console.log(removeLastFlavor(originalFlavors));
+ removeLastFlavor(originalFlavors);
+ console.log(originalFlavors);
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -131,9 +130,9 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(newarr,arr){
-    newarr= [...arr];
-    return newarr;
+function copy(newArr,arr){
+    newArr= [...arr];
+    return newArr;
 }
 console.log(copy(newFlavors, originalFlavors));
 
@@ -155,6 +154,7 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 hint - you can use the .includes method to help you solve this */
 
 function filterByWord(arr,flav){
+    // filteredArray=[]
     for (let i=0; i<arr.length; i++){
         if(arr[i].includes(flav)){
             console.log(arr[i]);
@@ -179,10 +179,10 @@ and should return the average number of words per item in the array.
 
 For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
 
-function getAverageWordLength(/*code here*/){
-
-    /*code here*/
-
+function getAverageWordLength(arr){
+    let total =0;
+    for(let i=0;)
+    total=
 }
 
 
