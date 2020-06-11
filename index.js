042be302c94d4c9c58cd5ec09e6252309fe58f12
arianahabas <arianahabas@gmail.com>
2020-06-11 15:49:37 -0500

    updated task 7

--- index.js
+++ index.js
@@ -156,12 +156,12 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 hint - you can use the .includes method to help you solve this */
 
 function filterByWord(arr,flav){
-    // filteredArray=[]
-    for (let i=0; i<arr.length; i++){
-        if(arr[i].includes(flav)){
-            console.log(arr[i]);
+    let filteredArray=[];
+    for (let i=0; i< arr.length; i++){
+        if (arr[i].includes(flav)) {
+            filteredArray.push(arr[i]);
         }
-     }
+     }return filteredArray
 }
 console.log(filterByWord(originalFlavors,"Lemon"));
 
